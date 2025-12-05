e1caabea47be2479a80571b3d5b8ddc96ab57826
NiaNia2 <lacodeuse@protonmail.com>
2025-12-05 10:59:44 +0100

    migration

--- migrations/Version20251205095832.php
+++ migrations/Version20251205095832.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251202211130 extends AbstractMigration
+final class Version20251205095832 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20251202211130 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE recette ADD file VARCHAR(255) DEFAULT NULL, ADD updated_at VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD pseudos VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE recette DROP file, DROP updated_at');
+        $this->addSql('ALTER TABLE `user` DROP pseudos');
     }
 }
